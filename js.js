1e3f7465b1446760706e7587d318806b770f9fbb
Ivab Ignjatović <50702092+MrBrabus@users.noreply.github.com>
2020-11-11 20:22:25 +0100

    Add files

---
+++ js.js
@@ -0,0 +1,8 @@
+function imgSlider(zamena){
+    document.querySelector('.pepsi').src = zamena;
+}
+
+function changeBgColor(color){
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
